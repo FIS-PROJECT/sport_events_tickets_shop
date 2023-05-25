a3c28879be1751d36ee544c4fff46179a4cbad90
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-05-25 21:19:59 +0300

    STOA-14 Adding user entities

---
+++ src/main/java/com/example/sporteventsticketsshop/entities/User.java
@@ -0,0 +1,73 @@
+package com.example.sporteventsticketsshop.entities;
+
+
+import org.dizitart.no2.objects.Id;
+import java.util.Objects;
+
+public class User {
+
+    @Id
+    public String username;
+    public String password;
+    public String role;
+
+    public User(){}
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(username, user.username)) return false;
+        return Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+}
