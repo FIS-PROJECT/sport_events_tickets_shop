58ba82be903ff4482b2515ff71326b119c264348
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-05-25 20:57:29 +0300

    STOA-10 Created a GUI Register Form

---
+++ src/main/java/com/example/sporteventsticketsshop/Main.java
@@ -0,0 +1,32 @@
+package com.example.sporteventsticketsshop;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setResizable(false);
+        stage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_files/register.fxml"));
+        primaryStage.setTitle("Sport Events!");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_files/" + fxml));
+        stage.getScene().setRoot(root);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
