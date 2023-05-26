7fe0ee33d88c919d6f535ed93dfa8a513fe3b3b5
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-05-26 12:57:40 +0300

    STOA-7 Add event backend functionality bug fixed

--- src/main/java/com/example/sporteventsticketsshop/controllers/AddEventController.java
+++ src/main/java/com/example/sporteventsticketsshop/controllers/AddEventController.java
@@ -1,19 +1,46 @@
 package com.example.sporteventsticketsshop.controllers;
 
 import com.example.sporteventsticketsshop.Main;
+import com.example.sporteventsticketsshop.database.NitriteDB;
+import com.example.sporteventsticketsshop.entities.SportType;
+import com.example.sporteventsticketsshop.exceptions.EventAlreadyExistException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.ResourceBundle;
 
-public class AddEventController {
+public class AddEventController implements Initializable {
+
     @FXML
-    private Label newEvent;
+    private TextField eventName;
 
+    @FXML
+    private TextField numberOfSeats;
+
+    @FXML
+    private DatePicker eventDate;
+
+    @FXML
+    private TextField ticketPrice;
+
+    @FXML
+    private ChoiceBox<SportType> sportTypeChoiceBox = new ChoiceBox<>();
     @FXML
     private Button back;
+
+    @FXML
+    private Button createEvent;
+
+    @FXML
+    private Label eventLabel;
+
+    private NitriteDB db = NitriteDB.getInstance();
 
     @FXML
     public void toOrganizerMenu(ActionEvent event) throws IOException {
@@ -21,4 +48,36 @@ public class AddEventController {
         m.changeScene("organizer-menu.fxml");
     }
 
+    @FXML
+    public void createEvent(ActionEvent event) {
+        checkEvent();
+    }
+
+    public void checkEvent() {
+        if(eventName.getText().isEmpty()) {
+            eventLabel.setText("Please fill event name!");
+        } else if(numberOfSeats.getText().isEmpty()) {
+            eventLabel.setText("Please fill number of seats!");
+        } else if(eventDate.getValue().isBefore(LocalDate.now())) {
+            eventLabel.setText("Change event date!");
+        } else if(ticketPrice.getText().isEmpty()) {
+            eventLabel.setText("Please fill ticket price!");
+        } else {
+            try {
+                double price = Double.parseDouble(ticketPrice.getText());
+                int number = Integer.parseInt(numberOfSeats.getText());
+                db.insertEvent(eventName.getText(), sportTypeChoiceBox.getValue(), eventDate.getValue().toString(), number, price );
+                eventLabel.setText("Event created!");
+            } catch (EventAlreadyExistException eventAlreadyExistException) {
+                eventLabel.setText(eventAlreadyExistException.getMessage());
+            } catch (NumberFormatException numberFormatException) {
+                eventLabel.setText("Ticket price and number of seats should be a number!");
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        sportTypeChoiceBox.getItems().addAll(Arrays.asList(SportType.FOOTBALL, SportType.BASKETBALL, SportType.TENNIS, SportType.RUGBY));
+    }
 }
