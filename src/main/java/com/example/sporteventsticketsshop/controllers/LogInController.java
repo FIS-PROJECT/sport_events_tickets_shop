70e53959abb5d17475eb398c6ffc53f05045b8a7
cebucmihai <mihaicebuc36@gmail.com>
2023-05-26 10:56:26 +0300

    STOA-15 Implement Log In Backend Functionality

---
+++ src/main/java/com/example/sporteventsticketsshop/controllers/LogInController.java
@@ -0,0 +1,51 @@
+package com.example.sporteventsticketsshop.controllers;
+import com.example.sporteventsticketsshop.Main;
+import com.example.sporteventsticketsshop.database.NitriteDB;
+import com.example.sporteventsticketsshop.entities.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import java.io.IOException;
+
+public class LogInController {
+    @FXML
+    private Button button_login;
+    @FXML
+    private TextField tf_username;
+    @FXML
+    private PasswordField tf_password;
+    @FXML
+    private  Button button_sign_up;
+    @FXML
+    private  Label wrongLogin;
+
+    NitriteDB db = NitriteDB.getInstance();
+
+    public void userLogin(ActionEvent event) throws IOException {
+        checkLogin();
+    }
+    @FXML
+    private void checkLogin() throws IOException{
+
+        if(tf_username.getText().isEmpty() ) {
+            wrongLogin.setText("Please fill in the username field");
+        }
+        else if(tf_password.getText().isEmpty()) {
+            wrongLogin.setText("Please fill in the password field");
+        }
+        else if("DACA USER-UL A FOST GASIT - FUNCTIONALITATE"==null) {
+
+        }
+        else {
+            wrongLogin.setText("User credentials are introduced wrong!");
+        }
+
+
+    }
+    public void toRegister(ActionEvent event) throws IOException{
+        Main m = new Main();
+        m.changeScene("register.fxml");
+    }
+
+}
+
